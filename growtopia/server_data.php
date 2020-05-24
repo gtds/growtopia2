1833c53a06d1da63c1d9153b5757062e29151fbe
u <58615943+darkisgay@users.noreply.github.com>
2020-05-24 10:53:57 +0700

    Create server_data.php

---
+++ growtopia/server_data.php
@@ -0,0 +1,9 @@
+server|127.0.0.1
+port|17091
+type|1
+#maint|Server is under maintenance. We will be back online shortly. Thank you for your patience!
+beta_server|127.0.0.1
+beta_port|17091
+beta_type|1
+meta|mymom.com
+RTENDMARKERBS1001
